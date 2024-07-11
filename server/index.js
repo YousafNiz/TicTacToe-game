c58193c7b6c5f980c7e7679cd1d3b2d4dc13ca09
YousafNiz <149098180+YousafNiz@users.noreply.github.com>
2024-07-11 22:22:08 +0500

    new code in nodejs

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log('yousaf')
+
+const exp = require('constants');
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+
+app.use(express.json());
+
+const DB = '';
+
+mongoose.connect(DB).then(() => {
+    console.log('connetion successful!');
+}).catch((e) => {
+    console.log(e);
+})
+
+server.listen(port, '0.0.0.0',() => {
+    console.log(`Server started and running on port ${port}`)
+});
